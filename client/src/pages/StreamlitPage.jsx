833a897feb8671713df34c2b23361b0df063d2bb
Jeel <shahjeel459@gmail.com>
2025-05-03 00:11:49 +0530

    modified

--- client/src/pages/StreamlitPage.jsx
+++ client/src/pages/StreamlitPage.jsx
@@ -7,10 +7,15 @@ const StreamlitPage = () => {
       <iframe
         src="http://localhost:8501"
         width="100%"
-        height="600px"
+        height="800px"
         frameBorder="0"
         title="Streamlit App"
       />
+      <noscript>
+        <p className="text-red-500 mt-4">
+          Please enable JavaScript to view the Streamlit dashboard.
+        </p>
+      </noscript>
     </div>
   );
 };
